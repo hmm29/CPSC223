395f3efd54d508176e0d005b05bed4f5bb5d66c7
Harrison Miller <hmaxmiller@gmail.com>
2016-02-29 02:07:12 -0500

    a

--- pset3/Subst16.c
+++ pset3/Subst16.c
@@ -250,6 +250,7 @@ int main(int argc, char *argv[])
             for(int i = 0; i < strlen(input); i++) {
                 res = str_replace(input, currentRulePtr->FROM, currentRulePtr->TO, currentRulePtr->filter);
                 if (input && res && strcmp(input, res) == 0) {  // if no change
+                    free(input);
                     free(res);
                     if(currentRulePtr->onFailureRuleIndex < numRules && currentRulePtr->onFailureRuleIndex > -1) {
                         if(currentRulePtr->onFailureRuleIndex > -1) {
