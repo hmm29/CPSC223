629849981abc7553473cd55f6de229eb11cff07c
Harrison Miller <hmaxmiller@gmail.com>
2016-02-29 05:07:37 -0500

    a

--- pset3/Subst16.c
+++ pset3/Subst16.c
@@ -170,7 +170,7 @@ char *str_replace(char *orig, char *from, char *to, char flag) {
         int i = 0;
         while (count--) {
             printf("to_cpy  in loop is now %s when count is %d\n", matches[count], count);
-            if(matches[count]) tmp_cpy = matches[count];
+            if(matches[count]) to_cpy = matches[count];
             ins = StrStr(orig, from);
             len_front = ins - orig;
             tmp = strncpy(tmp, orig, len_front) + len_front;
