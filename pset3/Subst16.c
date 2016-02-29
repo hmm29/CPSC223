c2d5cee774cf0c209d238ccbec1b6b55df373536
Harrison Miller <hmaxmiller@gmail.com>
2016-02-29 04:01:50 -0500

    a

--- pset3/Subst16.c
+++ pset3/Subst16.c
@@ -282,6 +282,7 @@ int main(int argc, char *argv[])
                     }
                     free(input);
                     input = res;
+                    free(res);
                 }
             }
 
