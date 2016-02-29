1c32f59a21e7fb0fff65c0a3d2e856d49ee113d1
Harrison Miller <hmaxmiller@gmail.com>
2016-02-29 02:46:51 -0500

    a

--- pset3/Subst16.c
+++ pset3/Subst16.c
@@ -142,7 +142,6 @@ char *str_replace(char *orig, char *from, char *to, char flag) {
             free(matched);
         }
 
-        if(result) free(result);
         tmp = result = malloc(strlen(orig) + (len_to - len_from) * count + 1);
 
         for(int i = 0; i < strlen(orig) + (len_to - len_from) * count + 1; i++) {
@@ -160,7 +159,7 @@ char *str_replace(char *orig, char *from, char *to, char flag) {
 
             // printf("result at end is now %s because orig is %s\n", result, orig);
         }
-                free(tmp);
+        strcpy(tmp, orig);
     }
     else if(flag == 'r') {
         int len_tmp;
