4833c0a0c22abc980441beb499132159cdd93417
Harrison Miller <hmaxmiller@gmail.com>
2016-05-14 19:53:56 -0400

    Rethink logic for boggle board

--- pset7/hash.c
+++
@@ -1,5 +0,0 @@
-/*
-File: boggle.c
-Description: This file contains the implementation of the hash table DS and operations for Boggle data storage.
-Name: Harrison Miller, hmm29
-*/
--- pset7/hash.h
+++ pset7/hash.h
@@ -8,6 +8,28 @@
 #ifndef HASH_H
 #define HASH_H
 
+struct entry {
+	char *key;
+	char *value;
+  struct entry *next;
+};
+
+typedef struct entry entry_t;
+
+struct hashtable {
+	int size;
+	struct entry **table;
+};
+
+typedef struct hashtable hashtable_t;
+
+hashtable_t *ht_create(int size);
+entry_t *ht_newpair(char *key, int value);
+int ht_hash( hashtable_t *hashtable, char *key);
+void ht_set(hashtable_t *hashtable, char *key, int value);
+int ht_get(hashtable_t *hashtable, char *key);
+void ht_sort(hashtable_t *hashtable);
+void ht_print_values( hashtable_t *hashtable, int showNonBoggle);
 
 #endif
 /* end HASH_H */
